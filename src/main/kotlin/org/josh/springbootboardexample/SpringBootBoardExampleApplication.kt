ae0f76424d5f8a443363a5e45b24e206a7aa6d7c
2hyjun <biper94@gmail.com>
2020-04-15 17:34:50 +0900

    feat: initial configuration

---
+++ src/main/kotlin/org/josh/springbootboardexample/SpringBootBoardExampleApplication.kt
@@ -0,0 +1,11 @@
+package org.josh.springbootboardexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootBoardExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootBoardExampleApplication>(*args)
+}
---
+++ src/test/kotlin/org/josh/springbootboardexample/SpringBootBoardExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.josh.springbootboardexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootBoardExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
